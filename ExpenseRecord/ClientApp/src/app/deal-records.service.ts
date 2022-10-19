018e3d72d5f6ccf40aa03a761726e7ca886a1e2f
xma12 <xma12@slb.com>
2022-10-19 17:13:14 +0800

    feat: change service to loacal storage

--- ExpenseRecord/ClientApp/src/app/deal-records.service.ts
+++ ExpenseRecord/ClientApp/src/app/deal-records.service.ts
@@ -6,40 +6,6 @@ import { v4 as uuidv4 } from 'uuid';
   providedIn: 'root'
 })
 export class DealRecordsService {
-  // mockRecords:Record[]=[
-  //   {
-  //     description:'lunch',
-  //     type:"meal",
-  //     amount:"50",
-  //     date:"20221010"
-  //   },
-  //   {
-  //     description:'lunch2',
-  //     type:"meal",
-  //     amount:"50",
-  //     date:"20221010"
-  //   },
-  //   {
-  //     description:'lunch3',
-  //     type:"meal",
-  //     amount:"50",
-  //     date:"20221010"
-  //   },
-  // ]
-
-  // constructor() { }
-  // fetchRecords(){
-  //   return this.mockRecords;
-  // }
-
-  // addRecord(newRecord:Record){
-  //   this.mockRecords.unshift(newRecord);
-  // }
-
-  // deleteRecord(deletingRecord:Record){
-  //   const targetIndex =this.mockRecords.findIndex(item=>item.description = deletingRecord.description);
-  //   this.mockRecords.splice(targetIndex, 1);
-  // }
   mockRecords:Record[]=[];
 
   constructor() { }
